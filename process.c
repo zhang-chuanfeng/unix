729920c1dd6fdbb3a389d9d8c3f46ecbc31040d8
zhang-chuanfeng <654300242@qq.com>
2023-12-03 22:18:44 +0800

    [ADD]signal

--- process.c
+++ process.c
@@ -1,14 +1,17 @@
 #include "apue.h"
 #include <sys/wait.h>
 
+static void sig_int(int);
 
 int main(void) {
     char buf[MAXLINE];
     pid_t pid;
     int status;
 
-    printf("%% ");
+    if (signal(SIGINT, sig_int) == SIG_ERR)
+        err_sys("signal error");
 
+    printf("%% ");
     while (fgets(buf, MAXLINE, stdin) != NULL) {
         if (buf[strlen(buf) - 1] == '\n')
             buf[strlen(buf) - 1] = 0;
@@ -29,4 +32,9 @@ int main(void) {
     }
 
     exit(0);
+}
+
+void sig_int(int signo) {
+    printf("interrupt\n%% ");
+    fflush(stdout);
 }
