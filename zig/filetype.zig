b76f5e7b2c1e51bf7b47ab5e0bf5a4e4bf50cf46
zhangchuanfeng <654300242@qq.com>
2024-04-29 16:27:45 +0800

    [MOD]修改获取文件类型

--- zig/filetype.zig
+++ zig/filetype.zig
@@ -4,11 +4,15 @@ pub fn main() !void {
     var args = std.process.args();
     _ = args.next();
     while (args.next()) |arg| {
-        const file = std.fs.openFileAbsolute(arg, .{}) catch |err| {
+        const file = std.os.open(arg, std.os.O.RDONLY, 0) catch |err| {
             std.debug.print("{s}: {}\n", .{ arg, err });
             continue;
         };
-        const stat = try file.stat();
+        defer std.os.close(file);
+
+        var st = std.mem.zeroes(std.os.system.Stat);
+        _ = std.os.system.lstat(arg, &st);
+        const stat = std.fs.File.Stat.fromSystem(st);
         const str = switch (stat.kind) {
             .block_device => "block device",
             .character_device => "char device",
