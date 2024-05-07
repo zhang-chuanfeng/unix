28f15056b778f1eae0a570270e30a0f4b7311a42
zhangchuanfeng <654300242@qq.com>
2024-05-07 11:46:17 +0800

    [MOD]调整zig filetype

--- zig/filetype.zig
+++ zig/filetype.zig
@@ -4,14 +4,13 @@ pub fn main() !void {
     var args = std.process.args();
     _ = args.next();
     while (args.next()) |arg| {
-        const file = std.os.open(arg, std.os.O.RDONLY, 0) catch |err| {
+        std.fs.accessAbsolute(arg, .{}) catch |err| {
             std.debug.print("{s}: {}\n", .{ arg, err });
             continue;
         };
-        defer std.os.close(file);
 
-        var st = std.mem.zeroes(std.os.system.Stat);
-        _ = std.os.system.lstat(arg, &st);
+        var st = std.mem.zeroes(std.posix.system.Stat);
+        _ = std.posix.system.lstat(arg, &st);
         const stat = std.fs.File.Stat.fromSystem(st);
         const str = switch (stat.kind) {
             .block_device => "block device",
