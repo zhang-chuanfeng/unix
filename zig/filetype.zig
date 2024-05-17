4a316906a098b239b324b116fdd953a04d4501f5
zhangchuanfeng <654300242@qq.com>
2024-05-17 10:52:50 +0800

    [MOD]filetype修改使用@tagName

--- zig/filetype.zig
+++ zig/filetype.zig
@@ -12,16 +12,6 @@ pub fn main() !void {
         var st = std.mem.zeroes(std.posix.system.Stat);
         _ = std.posix.system.lstat(arg, &st);
         const stat = std.fs.File.Stat.fromSystem(st);
-        const str = switch (stat.kind) {
-            .block_device => "block device",
-            .character_device => "char device",
-            .directory => "dir",
-            .file => "file",
-            .unix_domain_socket => "unix socket file",
-            .named_pipe => "name pipe file",
-            .sym_link => "sym link file",
-            else => "unknown file",
-        };
-        std.debug.print("{s}: {s}\n", .{ arg, str });
+        std.debug.print("{s}: {s}\n", .{ arg, @tagName(stat.kind) });
     }
 }
